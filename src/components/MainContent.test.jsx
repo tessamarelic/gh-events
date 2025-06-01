cfb0febfbbf0112cefa626fa356f739ce58bd521
Tessa Marelic <tessamarelic@gmail.com>
2025-06-01 16:23:16 +1000

    added comments [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -5,6 +5,7 @@ import userEvent from '@testing-library/user-event';
 import MainContent from './MainContent';
 
 // my tests
+// Add another comment here
 
 describe('MainContent', () => {
   it('should render a button', () => {
